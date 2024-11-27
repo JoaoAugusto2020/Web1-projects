39eec8fd327bea03022a7c1db677c6e198366822
João Augusto Marciano Silva <68918598+JoaoAugusto2020@users.noreply.github.com>
2024-11-27 16:52:20 -0300

    Add files via upload

--- agenda/controle/CadastrarContato_class.php
+++ agenda/controle/CadastrarContato_class.php
@@ -1,29 +1,33 @@
 <?php
-    include_once "modelo/ContatoDAO_class.php";
-
-    class CadastrarContato{
-
-        public function __construct(){
-            if(isset($_POST["enviar"])){
-                //formulário enviar foi enviado
-
-                $c = new Contato();
-                $c->setNome($_POST["nome"]);
-                $c->setEmail($_POST["email"]);
-                $c->setTelefone($_POST["telefone"]);
-                //$c->setFoto("qwe");
-
-                $dao = new ContatoDAO();
-                $dao->cadastrar($c);
-
-                $status = "Cadastro do COntato". $c->getNome() ."efetuado com sucesso";
-
-                $lista = $dao->listar();
-
-                include_once "visao/listaContato.php";
-            }else{
-                include_once "visao/formCadastroContato.php";
-            }
-        }
-    }
+	include_once("modelo/ContatoDAO_class.php");	
+	class CadastrarContato{
+		//CONTROLE
+	
+		public function __construct(){
+			
+			if(isset($_POST["enviar"])){
+				//formulário enviar foi enviado
+				
+				$c = new Contato();
+				$c->setNome($_POST["nome"]);
+				$c->setEmail($_POST["email"]);
+				$c->setTelefone($_POST["telefone"]);
+				
+				$dao = new ContatoDAO();
+				$dao->cadastrar($c);
+				
+				$status = "Cadastro do Contato " . $c->getNome() . 
+				" efetuado com sucesso";
+				
+				$lista = $dao->listar();
+				
+				include_once("visao/listaContato.php");
+				
+			} else{
+			
+				include_once("visao/formCadastroContato.php");	
+			
+			}
+		}
+	}
 ?>
--- agenda/visao/listaContato.php
+++ agenda/visao/listaContato.php
@@ -1,42 +1,56 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listagem de Contato</title>
-</head>
-<body>
-    <?php
-        if(isset($status)){
-            echo "<h2>" . $status . "<h2/>";
-        }
-    ?>
+<HTML>
 
-    <a href="contato.php?fun=cadastrar">Cadastrar</a>
-    <br><br>
+<HEAD>
+	<TITLE> Listagem de Contatos </TITLE>
+	<META charset="UTF-8" />
+</HEAD>
 
-    <table border="1px">
-        <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Email</th>
-            <th>Telefone</th>
-            <th><img src="visao/img/update.png" alt="update" width="30px"></th>
-            <th><img src="visao/img/delete.png" alt="delete" width="30px"></th>
-        </tr>
+<BODY>
+	<?php
+	if (isset($status)) {
+		echo "<H2>" . $status . "</H2>";
+	}
+	//Se $status está preenchida, imprimir ela
+	?>
+	<A href="contato.php?fun=cadastrar"> Cadastrar </A>
+	<br /><br />
 
-        <?php
-        foreach($lista as $c){
-            echo "<tr>";
-                echo "<td>" .$c->getID(). "</td>";
-                echo "<td><a href='contato.php?fun=exibir&id=" .$c->getID(). "'>" .$c->getNome(). "</a></td>";
-                echo "<td>" .$c->getEmail(). "</td>";
-                echo "<td>" .$c->getTelefone(). "</td>";
-                echo "<td><a href='contato.php?fun=alterar&id=" .$c->getID(). "'><img src='visao/img/update.png' alt='update' width='30px'></td>";
-                echo "<td><a href='contato.php?fun=excluir&id=" .$c->getID(). "'><img src='visao/img/delete.png' alt='delete' width='30px'></td>";
-            echo "</tr>";
-        }
-        ?>
-    </table>
-</body>
-</html>
+	<TABLE border="1px">
+		<TR>
+			<TH> ID </TH>
+			<TH> Nome </TH>
+			<TH> Email </TH>
+			<TH> Telefone </TH>
+			<TH> <img src="visao/img/update.png" width='30px' /> </TH>
+			<TH> <img src="visao/img/delete.png" width='30px' /> </TH>
+		</TR>
+		<!-- Primeira linha da tabela com o cabeçalho -->
+
+		<?php
+		foreach ($lista as $c) {
+			echo "<TR>";
+
+			echo "<TD>" . $c->getId() . "</TD>";
+
+			echo "<TD><A href='contato.php?fun=exibir&id=" . $c->getId() .
+				"'>" . $c->getNome() . "</A></TD>";
+
+			echo "<TD>" . $c->getEmail() . "</TD>";
+
+			echo "<TD>" . $c->getTelefone() . "</TD>";
+
+			echo "<TD><A href=contato.php?fun=alterar&id=" .
+				$c->getId() . "><img src='visao/img/update.png' width='30px'/> 
+						  </A></TD>";
+
+			echo "<TD><A href=contato.php?fun=excluir&id=" .
+				$c->getId() . "><img src='visao/img/delete.png' width='30px' /> 
+						  </A></TD>";
+
+			echo "</TR>";
+		}
+		?>
+	</TABLE>
+</BODY>
+
+</HTML>
